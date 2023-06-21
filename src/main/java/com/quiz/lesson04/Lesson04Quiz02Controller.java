13b7f539308ff9e282b223da317dc421cccf1558
DESKTOP-LV8U6T0\KANG SUNGTAE <sickc@naver.com>
2023-06-22 05:49:08 +0900

    06-21 과제

---
+++ src/main/java/com/quiz/lesson04/Lesson04Quiz02Controller.java
@@ -0,0 +1,38 @@
+package com.quiz.lesson04;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.quiz.lesson04.bo.RealtorBO;
+import com.quiz.lesson04.domain.Realtor;
+
+@Controller
+@RequestMapping("/lesson04/quiz02")
+public class Lesson04Quiz02Controller {
+
+	@Autowired
+	private RealtorBO realtorBO;
+	
+	@GetMapping("/add_realtor_view")
+	public String addRealtorView() {
+		return "lesson04/addRealtor";
+	}
+	
+	@PostMapping("/add_realtor")
+	public String addRealtor(
+			@ModelAttribute Realtor realtor,
+			Model model) {
+		
+		realtorBO.addRealtor(realtor);
+		
+		Realtor latestRealtor = realtorBO.getRealtorById(realtor.getId());
+		model.addAttribute("result", latestRealtor);
+		
+		return "lesson04/afterAddRealtor";
+	}
+}
