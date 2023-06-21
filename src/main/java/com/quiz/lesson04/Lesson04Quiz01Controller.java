f92a6568c3d0af81944d670a86bd960dcb23e51b
test <test@test.com>
2023-06-21 18:40:28 +0900

    0621 퀴즈

--- src/main/java/com/quiz/lesson04/Lesson04Quiz01Controller.java
+++ src/main/java/com/quiz/lesson04/Lesson04Quiz01Controller.java
@@ -2,8 +2,14 @@ package com.quiz.lesson04;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.quiz.lesson04.bo.SellerBO;
+import com.quiz.lesson04.domain.Seller;
 
 @RequestMapping("/lesson04/quiz01")
 @Controller
@@ -12,9 +18,50 @@ public class Lesson04Quiz01Controller {
 	@Autowired
 	private SellerBO sellerBO;
 	
-	@RequestMapping(path = "/add_seller_view", method = RequestMethod.GET)
+	// 판매자 추가 페이지
+	@GetMapping("/add_seller_view")
 	public String addSellerView() {
-		return "/lesson04/addSeller";
+		return "lesson04/addSeller";
 	}
 	
+	// 추가 action
+	@PostMapping("/add_seller")
+	public String addSeller(
+			@RequestParam("nickname") String nickname,
+			@RequestParam(value = "profile_image_url", required = false) String profileImageUrl,
+			@RequestParam("temperature") double temperature) {
+		
+		// DB insert
+		sellerBO.addSeller(nickname, profileImageUrl, temperature);
+		
+		// 입력 성공 페이지
+		return "lesson04/afterAddSeller";
+	}
+	
+	// 최근 가입자 정보 페이지
+	@GetMapping("/seller_info")
+	public String sellerInfo(Model model,
+			@RequestParam(value = "id", required = false) Integer id) {
+		// 최근 가입자 db select
+		
+		Seller seller = null;
+		
+		if(id != null) {
+			seller = sellerBO.getSellerById(id);
+		} else if (id == null) {
+			seller = sellerBO.getLatestSeller();
+		}
+		model.addAttribute("seller", seller);
+//		model.addAttribute("", seller.getNickname());
+//		model.addAttribute("", seller);
+		
+		return "lesson04/sellerInfo";
+	}
 }
+
+
+
+
+
+
+
--- src/main/webapp/WEB-INF/jsp/lesson04/sellerInfo.jsp
+++ src/main/webapp/WEB-INF/jsp/lesson04/sellerInfo.jsp
@@ -4,31 +4,17 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>판매자 추가</title>
+<title>가입자 정보</title>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
 </head>
 <body>
 	<div class="container">
-		<h1><b>판매자 추가</b></h1>
-		
-		<form method="post" action="/lesson04/quiz01/add_seller">
-			<div calss="form-group mt-2">
-				<span>닉네임</span>
-				<input type="text" name="nickname" class="form-control col-3">
-			</div>
-			<div calss="form-group mt-2">
-				<span>프로필 사진 url</span>
-				<input type="text" name="profile_image_url" class="form-control col-9">
-			</div>
-			<div calss="form-group mt-2">
-				<span>매너 온도</span>
-				<input type="text" name="temperature" class="form-control col-3">
-			</div>
-			
-			<input type="submit" class="btn btn-primary" value="추가">
-		</form>
+		<h1>가입자 정보</h1>
+		<img src="${seller.profileImageUrl}" alt="프로필 이미지" width="300">
+		<div class="display-3">${seller.nickname}</div>
+		<div class="display-4 text-warning font-weight-bold">${seller.temperature}</div>
 	</div>
 </body>
 </html>
